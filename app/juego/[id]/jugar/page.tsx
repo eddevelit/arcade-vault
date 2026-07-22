d019cdb3fd4058132c3f37a380f504c335af0911
Eduardo Ocampo <edd.develit@gmail.com>
2026-07-21 18:41:15 -0600

    feat: add AsteroidsGame component to GamePlayerPage for asteroides game

--- app/juego/[id]/jugar/page.tsx
+++ app/juego/[id]/jugar/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import { GAMES } from "@/lib/data";
 import GamePlayer from "@/components/GamePlayer";
+import AsteroidsGame from "@/components/AsteroidsGame";
 
 export default async function GamePlayerPage({
   params,
@@ -10,6 +11,8 @@ export default async function GamePlayerPage({
   const { id } = await params;
   const game = GAMES.find((g) => g.id === id);
   if (!game) notFound();
+
+  if (game.id === "asteroides") return <AsteroidsGame game={game} />;
 
   return <GamePlayer game={game} />;
 }
